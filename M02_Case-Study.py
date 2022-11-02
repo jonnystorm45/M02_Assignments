67a9e932d22032b5f6a1f70b0c0307d2629e30c9
jonnystorm45 <89853209+jonnystorm45@users.noreply.github.com>
2022-11-01 22:39:31 -0400

    Add files via upload

---
+++ M02_Case-Study.py
@@ -0,0 +1,34 @@
+# Jonathan Storm Anderson
+# M02_Case_Study.py
+# This App will allow the user to enter either the last name and then first name of a student to test
+# a students GPA against the Deans List and Honor Roll requirements or input "ZZZ" and end the program.
+# There will also be an option to continue to enter students names and GPAs until there are no more to test.
+
+def main():
+    global Student_First_Name
+    Student_Last_Name = input("Please Enter Student's Last Name or 'ZZZ' to End Program - ")
+
+    if Student_Last_Name == "ZZZ":
+        print("Program Ended")
+        exit()
+    else:
+        Student_First_Name = input("PLease Enter Student's First Name - ")
+
+    Student_GPA = float(input("Please Enter Student's GPA on a 4.0 Scale -  "))
+
+    if Student_GPA >= 3.5:
+        print(Student_First_Name, Student_Last_Name, "has made the Dean's List!")
+    elif Student_GPA >= 3.25:
+        print(Student_First_Name, Student_Last_Name, "has made the Honor Roll!")
+    else:
+        print(Student_First_Name, Student_Last_Name, "has not made any lists.")
+
+    restart = input("More Students GPAs to Test? ").lower()
+    if restart == "yes":
+        main()
+    else:
+        print("Thank you! Goodbye!")
+        exit()
+
+
+main()

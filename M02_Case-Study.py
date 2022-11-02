2c6294c8c8568598023a390d1cf08b8b9456dd1b
jonnystorm45 <89853209+jonnystorm45@users.noreply.github.com>
2022-11-02 16:56:20 -0400

    Add files via upload

--- M02_Case-Study.py
+++ M02_Case-Study.py
@@ -8,24 +8,31 @@ def main():
     global Student_First_Name
     Student_Last_Name = input("Please Enter Student's Last Name or 'ZZZ' to End Program - ")
 
-    if Student_Last_Name == "ZZZ":
+    while Student_Last_Name == "ZZZ":
         print("Program Ended")
+        break
         exit()
-    else:
+
+    if Student_Last_Name != "ZZZ":
+
         Student_First_Name = input("PLease Enter Student's First Name - ")
 
-    Student_GPA = float(input("Please Enter Student's GPA on a 4.0 Scale -  "))
+        Student_GPA = float(input("Please Enter Student's GPA on a 4.0 Scale -  "))
 
-    if Student_GPA >= 3.5:
-        print(Student_First_Name, Student_Last_Name, "has made the Dean's List!")
-    elif Student_GPA >= 3.25:
-        print(Student_First_Name, Student_Last_Name, "has made the Honor Roll!")
-    else:
-        print(Student_First_Name, Student_Last_Name, "has not made any lists.")
+        if Student_GPA >= 3.5:
+            print(Student_First_Name, Student_Last_Name, "has made the Dean's List!")
+        elif Student_GPA >= 3.25:
+            print(Student_First_Name, Student_Last_Name, "has made the Honor Roll!")
+        else:
+            print(Student_First_Name, Student_Last_Name, "has not made any lists.")
 
-    restart = input("More Students GPAs to Test? ").lower()
-    if restart == "yes":
-        main()
+        restart = input("More Students GPAs to Test? ").lower()
+        if restart == "yes":
+            main()
+        else:
+            print("Program Ended!")
+            print("Thank you! Goodbye!")
+            exit()
     else:
         print("Thank you! Goodbye!")
         exit()

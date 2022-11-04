d7bbb7720ea1a8aa5ae2bba135ffbc72d176f362
jonnystorm45 <89853209+jonnystorm45@users.noreply.github.com>
2022-11-04 19:03:07 -0400

    Add files via upload

---
+++ Chapter_4.2_Things_to_Do.py
@@ -0,0 +1,18 @@
+
+small = True
+green = True
+if small and green:
+    print("Pea")
+elif small and not green:
+    print("Cherry")
+elif green and not small:
+    print("Watermelon")
+elif not green and not small:
+    print("Pumpkin")
+
+
+
+
+
+
+
